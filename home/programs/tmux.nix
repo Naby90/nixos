ff756b8a119fef4cf5f6869609df466685c357d7
Naby90 <vadata77@gmail.com>
2025-07-20 05:53:33 +0200

    Added tmux

---
+++ home/programs/tmux.nix
@@ -0,0 +1,16 @@
+{ pkgs, config, ... }:
+{
+  programs.tmux = {
+    enable = true;
+    shortcut = "a";
+    # aggressiveResize = true; -- Disabled to be iTerm-friendly
+    baseIndex = 1;
+    newSession = true;
+    # Stop tmux+escape craziness.
+    escapeTime = 0;
+    # Force tmux to use /tmp for sockets (WSL2 compat)
+    secureSocket = false;
+
+    plugins = with pkgs; [
+      tmuxPlugins.better-mouse-mode
+    ];
